1134481b7c71cfa17021bf1681fa9cbdc10a7d2c
adala-wanyande <benardwanyande@gmail.com>
2024-01-24 23:45:18 +0300

    feat: implement About component

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section id="about" className="about-section" aria-label="About me">
+        <div className="sticky-container">
+            <h2 className="about-heading">About</h2>
+        </div>
+        <div className="about-content">
+            <p>
+                In 2019, I embarked on a journey into the world of software development, and since then, I've cultivated a deep 
+                passion for crafting innovative web and mobile solutions. Eager to continuously expand my skill set, I find joy 
+                in the process of learning and pushing the boundaries of what I can create.
+            </p>
+            <p>
+                My professional focus revolves around building impactful products and spearheading projects for clients. I've 
+                had the privilege of contributing to diverse sectors, having developed software for a 
+                <a href="https://www.willfreight.com/"> shipping agency</a>, lent my expertise to an 
+                <a href="https://www.ilabafrica.ac.ke/"> innovation lab</a>, and played a pivotal role in shaping the 
+                tech landscape for a dynamic startup.
+            </p>
+            <p>
+                Away from the keyboard, you'll often find me at the gym, channeling my energy into fitness, or on the football 
+                field, indulging in the beautiful game. Life isn't all about code; it's about balance.
+            </p>   
+        </div>
+    </section>   
+  )
+}
+
+export default About
