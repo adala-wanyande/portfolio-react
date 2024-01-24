a5c4c777c71ea4b046a66de3d4547e2bf6b8927e
adala-wanyande <benardwanyande@gmail.com>
2024-01-24 23:30:15 +0300

    feat: implement Intro component

---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <>
+        <h1>
+            <a href="/">Benard Adala Wanyande</a>
+        </h1>
+        <h2>Software Engineer</h2>
+        <p className="mini-about">I develop full-stack software solutions for both web and mobile platforms.</p>
+    </>
+  )
+}
+
+export default Intro
